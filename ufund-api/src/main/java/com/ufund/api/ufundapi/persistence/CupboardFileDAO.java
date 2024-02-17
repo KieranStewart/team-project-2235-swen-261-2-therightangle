4f1809a16fc9136c82850d7f5b112c0e5404c519
Logan <logannickerson21@gmail.com>
2024-02-17 12:55:37 -0500

    Fixed bean error thrown by JSON

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/CupboardFileDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/CupboardFileDAO.java
@@ -44,7 +44,7 @@ public class CupboardFileDAO implements CupboardDAO {
      * 
      * @throws IOException when file cannot be accessed
      */
-    public CupboardFileDAO(@Value("${needs.file}") String filename,ObjectMapper objectMapper) throws IOException {
+    public CupboardFileDAO(@Value("${cupboard.file}") String filename, ObjectMapper objectMapper) throws IOException {
         this.filename = filename;
         this.objectMapper = objectMapper;
         load();
