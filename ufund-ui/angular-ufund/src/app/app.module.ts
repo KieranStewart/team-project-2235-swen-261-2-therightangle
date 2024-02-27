459509fcd4462cd79368f644f821d992935272cd
Logan <logannickerson21@gmail.com>
2024-02-27 17:38:08 -0500

    Cupboard component stubbed out

--- ufund-ui/angular-ufund/src/app/app.module.ts
+++ ufund-ui/angular-ufund/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NeedComponent } from './need/need.component';
+import { CupboardComponent } from './cupboard/cupboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NeedComponent
+    NeedComponent,
+    CupboardComponent
   ],
   imports: [
     BrowserModule
---
+++ ufund-ui/angular-ufund/src/app/cupboard/cupboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Need } from '../need';
+import { NeedService } from '../need.service';
+
+@Component({
+  selector: 'app-cupboard',
+  templateUrl: './cupboard.component.html',
+  styleUrls: ['./cupboard.component.css']
+})
+export class CupboardComponent implements OnInit{
+  cupboard: Need[] = [];
+
+  constructor(private needService: NeedService) { }
+
+  ngOnInit(): void {
+    this.getCupboard();
+  }
+
+  getCupboard(): void {
+    this.needService.getCupboard()
+    .subscribe(cupboard => this.cupboard = cupboard);
+  }
+}
