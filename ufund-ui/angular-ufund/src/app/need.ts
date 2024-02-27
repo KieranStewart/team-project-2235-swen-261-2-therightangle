c062c9a1f6d609531616dc2e10ee616b2954a433
Logan <logannickerson21@gmail.com>
2024-02-26 21:55:28 -0500

    Created date.ts, updated need.ts

--- ufund-ui/angular-ufund/src/app/need.ts
+++ ufund-ui/angular-ufund/src/app/need.ts
@@ -1,8 +1,10 @@
+import { Date } from "./date";
+
 export interface Need {
     name: String;
     description: String;
     goal: number;
     progress: number;
-    
-
+    volunteerDates: Date;
+    deadline: Date;
 }
