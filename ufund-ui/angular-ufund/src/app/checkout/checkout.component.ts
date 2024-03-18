bd675c2c67c58ba90a1c06840fecd9ed14f2e525
Kieran <kieranmstewart@gmail.com>
2024-03-18 19:55:48 -0400

    Small push

--- ufund-ui/angular-ufund/src/app/checkout/checkout.component.ts
+++ ufund-ui/angular-ufund/src/app/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { CupboardComponent } from '../cupboard/cupboard.component';
 import { BasketService } from '../basket.service';
 import { Router } from '@angular/router';
+import { NeedService } from '../need.service';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -11,7 +12,7 @@ import { Router } from '@angular/router';
 
 export class CheckoutComponent implements OnInit{
 
-  constructor(private basketService: BasketService, private router: Router) { }
+  constructor(private basketService: BasketService, private router: Router, private needService: NeedService) { }
   
   total: number = 0;
 
@@ -32,8 +33,9 @@ export class CheckoutComponent implements OnInit{
     {
       for (const item of this.basketService.contents)
       {
-        item.progress = item.donationAmount;
+        item.progress = item.progress + +item.donationAmount;
         item.donationAmount = 0;
+        this.needService.updateNeed(item) // Causes duplicates and bad things to happen. (adding this comment fixed this (BEAN type error))
       }
       this.basketService.clear();
       this.router.navigate(['/confirmation/success']);
