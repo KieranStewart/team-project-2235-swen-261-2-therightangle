e65df4e9ad6ec6a499e526e149ea2294d8a956d6
Logan <logannickerson21@gmail.com>
2024-04-02 15:55:31 -0400

    Validation

--- ufund-ui/angular-ufund/src/app/need-edit/need-edit.component.ts
+++ ufund-ui/angular-ufund/src/app/need-edit/need-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Need } from "../need";
 import { NeedService } from "../need.service";
+import { take } from "rxjs";
 
 /**
  * Manager is able to make changes to need
@@ -11,6 +12,8 @@ import { NeedService } from "../need.service";
     styleUrls: ['./need-edit.component.css']
 })
 export class NeedEditComponent implements OnInit {
+
+    validTypes: String[] = ["donation", "volunteer"];
 
     constructor(private needService: NeedService) {}
 
@@ -36,28 +39,71 @@ export class NeedEditComponent implements OnInit {
     errorText: string = "";
 
     updateNeed(): void {
-
-    }
-
-    changeName(newName: string): void {
-        this.currentNeed.name = newName;
-        this.update();
-    }
-
-    changeGoal(newGoal: number): void {
-        if(newGoal < 0) {
-
+        const that = this;
+        this.errorText = "Loading...";
+        if(this.validateNeed()) {
+            this.needService.updateNeed(this.currentNeed).pipe(take(1)).subscribe({
+                next(value) {
+                    if(value == null) {
+                        that.errorText = "There was an error updating this need.";
+                    } else {
+                        that.errorText = "Successfully updated!";
+                    }
+                }
+            });
         }
-        this.currentNeed.goal = newGoal;
-        this.update();
     }
 
-    changeDescription(newDescription: string) {
-        this.currentNeed.description = newDescription;
-        this.update();
+    private validateNeed(): boolean {
+        if(this.currentNeed.name.length == 0) {
+            this.errorText = "Please enter a name";
+            return false;
+        }
+        if(this.currentNeed.deadline.day <= 0 || this.currentNeed.deadline.day > 31) {
+            this.errorText = "Invalid day";
+            return false;
+        }
+        if(this.currentNeed.deadline.month <= 0 || this.currentNeed.deadline.month > 12) {
+            this.errorText = "Invalid month";
+            return false;
+        }
+        if(this.currentNeed.deadline.year == 0) {
+            this.errorText = "Please check your year";
+            return false;
+        }
+        if(!this.validTypes.includes(this.currentNeed.type)) {
+            this.errorText = "Invalid need type";
+            return false;
+        }
+
+        if(this.currentNeed.volunteerDates.length > 0 && this.currentNeed.type == this.validTypes[0]) {
+            this.errorText = "There are volunteer dates for a donation-only need";
+            return false;
+        }
+        
+
+        if(this.currentNeed.goal < this.currentNeed.progress) {
+            this.errorText = "Goal is less than the amount collected";
+            return false;
+        }
+
+        for (let index = 0; index < this.currentNeed.volunteerDates.length; index++) {
+            const element = this.currentNeed.volunteerDates[index];
+            if(element.day <= 0 || element.day > 31) {
+                this.errorText = "Invalid day for volunteer date number " + (index + 1);
+                return false;
+            }
+            if(element.month <= 0 || element.month > 12) {
+                this.errorText = "Invalid month for volunteer date number " + (index + 1);
+                return false;
+            }
+            if(element.year == 0) {
+                this.errorText = "Please check your year for volunteer date number " + (index + 1);
+                return false;
+            }
+        }
+
+        return false;
     }
 
-    update(): void {
-        this.needService.updateNeed(this.currentNeed).subscribe().unsubscribe();
-    }
 }
