cf0350516236db8d51ac1a9ba536483c5b2d109d
ltdel <ltd7868@g.rit.edu>
2024-04-03 12:40:53 -0400

    need-component altered

--- ufund-ui/angular-ufund/src/app/need/need.component.ts
+++ ufund-ui/angular-ufund/src/app/need/need.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Need } from '../need';
 import { NeedCacheService } from '../need-cache.service';
+import { LoginService } from '../login.service';
+import { Account } from '../account';
+import { TagManagerService } from '../tag-manager.service';
+
 /*
  * This component is NOT a need object (need.ts is kind of that)
  * It is also NOT the thing that communicates needs
@@ -18,12 +22,42 @@ import { NeedCacheService } from '../need-cache.service';
 })
 export class NeedComponent {
 
-  constructor(private cacheService: NeedCacheService) { }
+  constructor(
+    private cacheService: NeedCacheService,
+    private loginService: LoginService,
+    private tagManagerService: TagManagerService) { }
 
   @Input()
   need!: Need;
 
+  currentUser: Account = this.loginService.userAccount
+  tagList: String[] = this.tagManagerService.getList();
+
   showDetails(): void {
     this.cacheService.selectedNeed = this.need;
+  }
+
+  adminTag(): boolean{ //admin tag is present and it's the only one
+    if(this.tagList.length==1 && this.tagList[0] == 'admin'){
+      return true;
+    }
+    return false;
+  }
+
+  userIsAdmin(): boolean {//check user status (admin or not)
+    return this.currentUser.isAdmin;
+  }
+
+  tagAndUser(): boolean{
+    if(this.adminTag() && this.userIsAdmin()){
+      return true;
+    } if(!this.adminTag() && this.userIsAdmin()){
+      return true;
+    } if(!this.adminTag() && !this.userIsAdmin()){
+      return true;
+    } if(this.adminTag() && !this.userIsAdmin){
+      return false;
+    }
+    return false;
   }
 }
