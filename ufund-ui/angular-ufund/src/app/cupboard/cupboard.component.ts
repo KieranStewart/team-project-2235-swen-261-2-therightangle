fcbae12dad038270d09caf717578f1827828baa4
Logan <logannickerson21@gmail.com>
2024-04-03 20:17:43 -0400

    Fixed search bug

--- ufund-ui/angular-ufund/src/app/cupboard/cupboard.component.ts
+++ ufund-ui/angular-ufund/src/app/cupboard/cupboard.component.ts
@@ -4,6 +4,7 @@ import { NeedService } from '../need.service';
 import { ActivatedRoute } from '@angular/router';
 import { NeedCacheService } from '../need-cache.service';
 import { take } from 'rxjs';
+import { HomeViewComponent } from '../home-view/home-view.component';
 
 
 @Component({
@@ -15,7 +16,7 @@ export class CupboardComponent implements OnInit{
   cupboard: Need[] = []; 
   title = 'angular-ufund';
 
-  constructor(private needService: NeedService, public needCacheService: NeedCacheService) { }
+  constructor(private needService: NeedService, public needCacheService: NeedCacheService, private searchComponent : HomeViewComponent) { }
   
   ngOnInit(): void {
     this.getCupboard();
@@ -27,6 +28,7 @@ export class CupboardComponent implements OnInit{
     .subscribe({
       next(cupboard) {
           that.cupboard = cupboard;
+          that.searchComponent.search(that.searchComponent.searchTerm);
           for (let index = 0; index < that.cupboard.length; index++) {
             const element = that.cupboard[index];
             if(that.needCacheService.selectedNeed != null && element.name == that.needCacheService.selectedNeed.name) {
