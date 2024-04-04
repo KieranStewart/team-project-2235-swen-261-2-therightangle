f187336e44e4ad494bbc35e0257d45442bf26a2a
Logan <logannickerson21@gmail.com>
2024-04-03 20:02:18 -0400

    Removed my random console logs

--- ufund-ui/angular-ufund/src/app/need-edit/need-edit.component.ts
+++ ufund-ui/angular-ufund/src/app/need-edit/need-edit.component.ts
@@ -71,7 +71,6 @@ export class NeedEditComponent {
     }
 
     private validateNeed(): boolean {
-        console.log(this.currentNeed);
         if(this.currentNeed == null) {
             return false;
         }
